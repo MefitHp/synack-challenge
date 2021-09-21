8c7ddb5ac088779f6775c8f7d71842f2f5f6ae7d
Mefit Hernández <mefitdev@gmail.com>
2021-09-20 23:29:10 -0500

    Addes SearchBar Styling and basic functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import styled from "@emotion/styled";
 
+import { SearchBar } from "./components";
 const Container = styled.section`
+  height: calc(100vh - 2em);
   max-width: 128;
   max-width: 960px;
   margin: 0 auto;
-  padding: 0 1em;
+  padding: 1em;
+`;
+
+const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const SearchBarContainer = styled.section`
+  padding: 1em;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+`;
+const ResultsContainer = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  flex: 1;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  overflow-y: auto;
 `;
 
 function App() {
   return (
     <Container className="App">
-      <h1>Synack</h1>
+      <Layout>
+        <SearchBarContainer>
+          <SearchBar />
+        </SearchBarContainer>
+        <ResultsContainer>Results</ResultsContainer>
+      </Layout>
     </Container>
   );
 }
